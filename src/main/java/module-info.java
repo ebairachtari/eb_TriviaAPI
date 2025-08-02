b60543e0bdd74822781d382d70c2ab211802e1dd
Evgenia Bairachtari <121518003+ebairachtari@users.noreply.github.com>
2025-08-02 15:26:09 +0200

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,6 @@ module eb.TriviaAPI {
     exports eb_erwtisidb;
     exports eb_exceptions;
 
-    
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.annotation;
